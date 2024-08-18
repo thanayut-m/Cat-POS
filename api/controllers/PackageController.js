7335255207645533a61e26a1fea583c04efbdf63
Thanayut Mungumklang <thanayut.mukl@gmail.com>
2024-08-18 21:43:56 +0700

    Create function
    - create function Double check password.
    - create function Check your registered email.

--- api/controllers/PackageController.js
+++ api/controllers/PackageController.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { PrismaClient, Prisma } = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 const app = express();
 const bcrypt = require("bcrypt");
 
 const prisma = new PrismaClient();
-Prisma.PrismaClientKnownRequestError;
 
 app.get("/package/list", async (req, res) => {
   try {
@@ -31,7 +30,7 @@ app.post("/package/users", async (req, res) => {
     });
 
     if (checke_mail.length > 0) {
-        return res.status(400).json({ message: "Email already exists." });
+      return res.status(400).json({ message: "Email already exists." });
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
@@ -47,6 +46,30 @@ app.post("/package/users", async (req, res) => {
   } catch (e) {
     console.error(e);
     res.status(500).json({ message: e.message });
+  }
+});
+
+app.post("/package/check-email", async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: "Email is required" });
+  }
+
+  const emailLowerCase = email.toLowerCase();
+
+  try {
+    const user = await prisma.users.findUnique({
+      where: { email: emailLowerCase },
+    });
+
+    if (user) {
+      return res.json({ exists: true });
+    } else {
+      return res.json({ exists: false });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 
--- web/my-app/src/pages/Package.jsx
+++ web/my-app/src/pages/Package.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import config from './../config';
@@ -8,10 +7,18 @@ import Swal from 'sweetalert2';
 function Package() {
   const [packages, setPackages] = useState([]);
   const [yourPackages, setYourPackages] = useState({});
-  const [password, setPassword] = useState();
-  const [email, setEmail] = useState();
-  const [storeName, setStoreName] = useState();
-  const [username, setUesrname] = useState();
+  const [storeName, setStoreName] = useState('');
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordErr, setPasswordErr] = useState('');
+
+  const [username, setUsername] = useState('');
+  const [first_Name, setFirst_Name] = useState('');
+  const [last_Name, setLast_Name] = useState('');
 
   useEffect(() => {
     fetchDataPackage();
@@ -33,8 +40,48 @@ function Package() {
     setYourPackages(item);
   }
 
+  useEffect(() => {
+    setUsername(`${first_Name} ${last_Name}`)
+  }, [first_Name, last_Name])
+
+  const checkEmailExists = async (email) => {
+    try {
+      const response = await axios.post(config.api_path + `/package/check-email`, { email });
+
+      if (response.status === 200) {
+        if (response.data.exists) {
+          console.log("อีเมลนี้ถูกใช้งานแล้ว");
+          return false;
+        } else {
+          console.log("อีเมลนี้ยังไม่ถูกใช้งาน");
+          return true;
+        }
+      } else {
+        console.error("เกิดข้อผิดพลาดในการตรวจสอบอีเมลล์");
+        return false;
+      }
+    } catch (err) {
+      console.error("ข้อผิดพลาด:", err.message);
+      return false;
+    }
+  };
+
   const handleSignUp = async (e) => {
     e.preventDefault();
+
+    const emailExists = await checkEmailExists(email);
+    if (emailExists) {
+      console.log("อีเมลนี้ถูกใช้งานแล้ว");
+      setEmailError("อีเมลนี้ถูกใช้งานแล้ว");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      console.log("รหัสผ่านไม่ตรงกัน");
+      setPasswordErr("รหัสผ่านไม่ตรงกัน");
+      return;
+    }
+
     try {
       Swal.fire({
         title: "ยืนยันการสมัคร",
@@ -42,7 +89,7 @@ function Package() {
         icon: 'question',
         showCancelButton: true,
         showConfirmButton: true
-      }).then(async (res) => { 
+      }).then(async (res) => {
         if (res.isConfirmed) {
           try {
             const payload = {
@@ -90,10 +137,11 @@ function Package() {
               <div className="card mt-3">
                 <div className="card-body text-center">
                   <div className="mt-3 h3 text-success">{item.package_name}</div>
+                  <hr />
                   <div className="mt-3 h5 text-secondary">{parseInt(item.bill_amount).toLocaleString('th-TH')}&nbsp;บิล / เดือน</div>
                   <div className="mt-3 h5 text-secondary">{parseInt(item.price).toLocaleString('th-TH')}&nbsp;บาท</div>
                   <div className="mt-3">
-                    <button onClick={e => choosePackage(item)} data-bs-toggle="modal" data-bs-target="#modalSignUp" className="btn btn-primary">สมัครสมาชิก</button>
+                    <button onClick={() => choosePackage(item)} data-bs-toggle="modal" data-bs-target="#modalSignUp" className="btn btn-primary">สมัครสมาชิก</button>
                   </div>
                 </div>
               </div>
@@ -107,21 +155,44 @@ function Package() {
             <div className="alert alert-info">{yourPackages.package_name} ราคา {yourPackages.price} </div>
           </div>
           <div className="mt-2">
-            <label htmlFor="">ชื่อร้าน</label>
-            <input className="mt-1 form-control" type="text" onChange={e => setStoreName(e.target.value)} />
-          </div>
-          <div className="mt-2">
-            <label htmlFor="">ชื่อ</label>
-            <input className="mt-1 form-control" type="text" onChange={e => setUesrname(e.target.value)} />
-          </div>
-          <div className="mt-2">
             <label htmlFor="">E-mail</label>
             <input className="mt-1 form-control" type="email" onChange={e => setEmail(e.target.value)} />
+            {emailError && (
+              <label className="block text-danger text-sm font-bold mt-1">
+                {emailError}
+              </label>
+            )}
+          </div>
+          <div className="row">
+            <div className="mt-2 col-6">
+              <label htmlFor="">รหัสผ่าน</label>
+              <input className="mt-1 form-control" type="password" onChange={e => setPassword(e.target.value)} />
+              {passwordErr && (
+                <label className="block text-danger text-sm font-bold">
+                  {passwordErr}
+                </label>
+              )}
+            </div>
+            <div className="mt-2 col-6">
+              <label htmlFor="">ยืนยันรหัสผ่าน</label>
+              <input className="mt-1 form-control" type="password" onChange={e => setConfirmPassword(e.target.value)} />
+            </div>
           </div>
           <div className="mt-2">
-            <label htmlFor="">รหัสผ่าน</label>
-            <input className="mt-1 form-control" type="password" onChange={e => setPassword(e.target.value)} />
+            <label htmlFor="">ชื่อร้านค้า</label>
+            <input className="mt-1 form-control" type="text" onChange={e => setStoreName(e.target.value)} />
           </div>
+          <div className="row">
+            <div className="mt-2 col-6">
+              <label htmlFor="">ชื่อ</label>
+              <input className="mt-1 form-control" type="text" onChange={e => setFirst_Name(e.target.value)} />
+            </div>
+            <div className="mt-2 col-6">
+              <label htmlFor="">นามสกุล</label>
+              <input className="mt-1 form-control" type="text" onChange={e => setLast_Name(e.target.value)} />
+            </div>
+          </div>
+
           <div className="mt-4">
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">ยกเลิก</button>
